ca94f76ff9ea0f0a2d59a469c42299ec22a9b85b
Rob Somers <64647442+robsomers900@users.noreply.github.com>
2024-12-06 16:29:15 +0000

    Create app.js and index.html

---
+++ app.js
@@ -0,0 +1,4 @@
+//function/promise to fetch the data via spotify api
+//start with camelot keys for mix well criterium - analagous to the knights
+//travails - camelot keys can be represented as [int, int] - e.g., a=1
+//looking for the long chains
